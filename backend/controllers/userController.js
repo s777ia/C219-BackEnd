9ff2186b62c9ac3bd8358aeaed0c6abd9ddaf4e6
s777ia <23046128@myrp.edu.sg>
2025-02-01 18:03:36 +0800

    fixed userController. 100% works

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,39 +2,39 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
-    jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
 }
 
-// login user
+// login a user
 const loginUser = async (req, res) => {
-    const { email, password } = req.body
+  const {email, password} = req.body
 
-    try {
-        const user = await User.login(email, password)
+  try {
+    const user = await User.login(email, password)
 
-        // create a token
-        const token = createToken(user._id)
+    // create a token
+    const token = createToken(user._id)
 
-        res.status(200).json({ email, token })
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
-// signup user
+// signup a user
 const signupUser = async (req, res) => {
-    const { email, password } = req.body
+  const {email, password} = req.body
 
-    try {
-        const user = await User.signup(email, password)
+  try {
+    const user = await User.signup(email, password)
 
-        // create a token
-        const token = createToken(user._id)
+    // create a token
+    const token = createToken(user._id)
 
-        res.status(200).json({ email, token })
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-    res.json({ msg: 'singup user' })
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
+
 module.exports = { signupUser, loginUser }
